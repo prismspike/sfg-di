a06a1ee948d22dba81a7410f65bfc34d62460645
Tobias <tobias.freymann@ish.de>
2021-04-01 16:25:09 +0200

    tutorial bis einschliesslich kurs kursinhalt 38

---
+++ src/main/java/tobi/fiasko/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,13 @@
+package tobi.fiasko.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the Primary Bean";
+    }
+}
